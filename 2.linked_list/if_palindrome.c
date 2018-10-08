079e65c40b828dbc8e464b78d53439eb9f848f0f
jacobicao <caozj1017@hotmail.com>
2018-10-08 13:44:59 +0800

    A practice of linked list: whether a word is a palindrome

---
+++ 2.linked_list/if_palindrome.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/malloc.h>
+typedef struct Node
+{
+  char data;
+  struct Node *next;
+}node;
+
+node* creat()
+{
+  node *head,*p,*q;
+  head=(node*)malloc(sizeof(node));
+  q=head;
+  puts("Insert the word:");
+  while(1)
+  {
+      char a; 
+      scanf("%c",&a);
+      if (a=='\n') 
+        break;
+      p=(node*)malloc(sizeof(node));
+      p->data=a;
+      q->next=p;
+      q=p;
+   }
+   q->next=NULL;
+   q=head->next;
+   free(head);
+   return(q);
+}
+
+void print(node *a)
+{
+  puts("print:");
+  while(a!=NULL)
+  {
+    printf("%c ",a->data);
+    a=a->next;
+  }
+  printf("\n");
+}
+
+node* reverseList(node *H)
+{
+  if (H==NULL || H->next==NULL) 
+      return H;
+  node *newHead=reverseList(H->next); 
+  H->next->next=H;
+  H->next=NULL;
+  return newHead;
+}
+
+int if_palindrome(node *a)
+{
+  node *fast,*slow;
+  fast=a;
+  slow=a;
+  while(fast!=NULL)
+  {
+    fast=fast->next;
+    if(fast==NULL) 
+      break;
+    fast=fast->next;
+    slow=slow->next;
+  }
+  fast=reverseList(slow);
+  while(a!=slow)
+  {
+    if (fast->data!=a->data)
+      return 0;
+    fast=fast->next;
+    a=a->next;
+  }
+  
+  return 1; 
+}
+
+int main(){
+  node *a=NULL;
+  a=creat();
+  if (if_palindrome(a))
+    printf("It is a palindrome.\n");
+  else
+    printf("It is not a palindrome.\n");
+}
